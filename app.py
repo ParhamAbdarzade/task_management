98fe7575a920bdf704326f6cfd3385119c78bf1b
Parham Abdarzade <parham-abdarzade@hotmail.com>
2024-02-11 13:30:15 +0330

    add fastapi_app - add Controller class to app.py - sync apis with controller

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-from fastapi import FastAPI
+from model import TaskModel
+from typing import Dict
 
 
-app = FastAPI()
+class Controller:
+    def __init__(self):
+        self.last_used_id = -1
+        self.task_db: Dict[int, TaskModel] = {}
+
+    def create_task(self, task: TaskModel):
+        self.last_used_id += 1
+        self.task_db[self.last_used_id] = task
+        return self.task_db[self.last_used_id]
+
+    def update_task(self, task_id, task: TaskModel):
+        if task_id not in self.task_db:
+            return None
+        self.task_db[task_id] = task
+        return self.task_db[task_id]
+
+    def read_task(self, task_id):
+        if task_id not in self.task_db:
+            return None
+        return self.task_db[task_id]
+
+    def delete_task(self, task_id):
+        if task_id not in self.task_db:
+            return None
+        return self.task_db.pop(task_id)
+
+    def read_all_tasks(self):
+        return self.task_db
+
+
+controller = Controller()
